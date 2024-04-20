46308a5c20e5a3deb0d95ed9ccd298db1f17fbd0
juba0713 <basasrepo@gmail.com>
2024-04-20 11:13:47 +0800

    Push CSS Folder and Images Folder

---
+++ Capstone-1/src/main/java/capstone/model/service/impl/OfficerServiceImpl.java
@@ -0,0 +1,17 @@
+package capstone.model.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import capstone.model.dto.OfficerInOutDto;
+import capstone.model.service.OfficerService;
+
+@Service
+public class OfficerServiceImpl implements OfficerService{
+
+	@Override
+	public OfficerInOutDto getAllApplicants() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
