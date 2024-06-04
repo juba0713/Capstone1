2cf7a45a38386a8768ac6f38e9217eea9851fa4d
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-06-04 23:57:19 +0800

    added dashboard in admin

--- Capstone-1/src/main/java/capstone/controller/AdminController.java
+++ Capstone-1/src/main/java/capstone/controller/AdminController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
     @GetMapping("/admin/home")
-    public String showTest() {
+    public String showAdminHome() {
         // This will return the name of the HTML file (without the .html extension)
         return "admin/home";
     }
 
+    @GetMapping("/admin/applicants")
+    public String showApplicants() {
+        // This will return the name of the HTML file (without the .html extension)
+        return "admin/applicants";
+    }
+
     @GetMapping("/admin/users")
-    public String showUserTest() {
+    public String showUsers() {
         // This will return the name of the HTML file (without the .html extension)
         return "admin/users";
     }
