040c84769e26f6920c647201146697c689401f8f
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-06-15 01:05:51 +0800

    added admin create user

--- Capstone-1/src/main/java/capstone/controller/AdminController.java
+++ Capstone-1/src/main/java/capstone/controller/AdminController.java
@@ -12,10 +12,10 @@ import capstone.model.service.AdminService;
 
 @Controller
 public class AdminController {
-	
-	@Autowired
-	private AdminService adminService;
-	
+
+    @Autowired
+    private AdminService adminService;
+
     @GetMapping("/admin/home")
     public String showAdminHome() {
         // This will return the name of the HTML file (without the .html extension)
@@ -24,23 +24,30 @@ public class AdminController {
 
     @GetMapping("/admin/applicants")
     public String showApplicants(@ModelAttribute AdminWebDto webDto) {
-    	
-    	AdminInOutDto outDto = adminService.getAllApplicants();
-    	
-    	webDto.setAllApplicants(outDto.getAllApplicants());
-    	
+
+        AdminInOutDto outDto = adminService.getAllApplicants();
+
+        webDto.setAllApplicants(outDto.getAllApplicants());
+
         // This will return the name of the HTML file (without the .html extension)
         return "admin/applicants";
     }
 
     @GetMapping("/admin/users")
     public String showUsers(@ModelAttribute AdminWebDto webDto) {
-    	
-    	AdminInOutDto outDto = adminService.getAllUsers();
-    	
-    	webDto.setAllUsers(outDto.getAllUsers());
-    	
+
+        AdminInOutDto outDto = adminService.getAllUsers();
+
+        webDto.setAllUsers(outDto.getAllUsers());
+
         // This will return the name of the HTML file (without the .html extension)
         return "admin/users";
+    }
+
+    @GetMapping("/admin/create-user")
+    public String createUser(@ModelAttribute AdminWebDto webDto) {
+
+        // This will return the name of the HTML file (without the .html extension)
+        return "admin/createUser";
     }
 }
